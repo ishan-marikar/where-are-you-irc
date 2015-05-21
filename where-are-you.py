dc029df3d31ecb1d17447f998630fb7296b2adba
Emma Jones <awwcookies@gmail.com>
2015-05-21 12:18:37 -0500

    Update where-are-you.py

--- where-are-you.py
+++ where-are-you.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import hexchat
-import re
-# PyNaCl issues a warning every time it's imported
-import requests
+import urllib.request
+import json
+import threading
 
 # Module details
 __module_name__ = "where-are-you"
@@ -13,35 +13,19 @@ __module_description__ = "Find the location of everyone joining the channel."
 
 # Global variables
 geoip_api = "https://freegeoip.net/json/"
-ip_pattern = re.compile("[0-9]+(?:\.[0-9]+){3}")
 
 def join_message_parser(word, word_eol, userdata):
-	try:
-		# Use the regex to find any IPs in the message
-		# TODO: Searching for hostnames if we don't find any IPs
-		ip = re.findall(ip_pattern, word_eol[0])
-		# Loop through all the IPs and append the last one to the url
-		for current_ip in ip:
-			complete_url = geoip_api + current_ip
-		# Send the request to the API and parse the response
-		response = requests.get(complete_url)
-		location = response.json()
-		# Loop through the  response and change any empty values to 'N/A'
-		for element in location:
-			if not location[element]:
-				location[element] = "N/A"
-		# Print the message onto the Hexchat buffer
-		print "\002\00304 ( %s ) Country: %s , Region: %s , City: %s , Time Zone: %s " % ( word[0], location['country_name'], location['region_name'], location['city'], location['time_zone'])
-
-	except:
-		# Silently pass through any errors (which is more likely to happen because sometimes
-		# the thier host might not be their machine IP and more likely be a bouncer or what-not
-		pass
-	# Report back to Hexchat letting it know that it shouldn't do anything to the message we
-	# intercepted in the buffer
+	ip = word_eol[1].split("@")[1]
+	temp = urllib.request.urlopen("{}{}".format(geoip_api, ip)).read()
+	location = json.loads(temp.decode("utf-8"))
+	print("\002\00304 ( {} ) Country: {} , Region: {} , City: {} , Time Zone: {} ".format(
+		word[0], location['country_name'], location['region_name'], location['city'], location['time_zone'])
+	)
 	return hexchat.EAT_NONE
 
-print "\002%s is loading up .." % __module_name__
-hexchat.hook_print("Join", join_message_parser)
-hexchat.hook_print("WhoIs Real Host", join_message_parser)
+def thread(word, word_eol, userdata):
+	threading.Thread(target=join_message_parser, args=(word, word_eol, userdata)).start()
 
+print("\002%s is loading up .." % __module_name__)
+hexchat.hook_print("Join", thread)
+hexchat.hook_print("WhoIs Real Host", thread)
